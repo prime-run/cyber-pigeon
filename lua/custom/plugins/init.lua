ca7e9f10ac146683bc798f562ca8daea9a2aca15
prime-run <ashkan.samadiyan@gmail.com>
2025-04-10 15:17:07 +0000

    orgnied files and removed extra files

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,16 +1,1 @@
-return {
-  {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {
-      view_options = {
-        show_hidden = true,
-      },
-      buf_options = {
-        buflisted = false,
-        bufhidden = 'hide',
-      },
-    },
-  },
-}
+return {}
